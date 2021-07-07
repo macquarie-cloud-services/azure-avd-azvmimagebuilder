e9d8af2fe8f8e3b54fd1682c80760b7cc5dc4482
rphoon <49342053+rphoon@users.noreply.github.com>
2021-07-07 23:44:58 +1000

    Update 1_Optimize_OS_for_WVD.ps1

--- 1_Optimize_OS_for_WVD.ps1
+++ 1_Optimize_OS_for_WVD.ps1
@@ -11,23 +11,21 @@
  Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
  Write-output 'AIB Customization: Starting OS Optimizations script'
  Expand-Archive -LiteralPath 'C:\\Optimize\\Windows_10_VDI_Optimize-master.zip' -DestinationPath $Localpath -Force -Verbose
- Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+ # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
  Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-main
  
  # instrumentation
  $osOptURL = 'https://raw.githubusercontent.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/master/Win10_VirtualDesktop_Optimize.ps1'
  $osOptURLexe = 'optimize.ps1'
  Invoke-WebRequest -Uri $osOptURL -OutFile $osOptURLexe
- 
- 
+  
  # Patch: overide the Win10_VirtualDesktop_Optimize.ps1 - setting 'Set-NetAdapterAdvancedProperty'(see readme.md)
  Write-output 'Patch: Disabling Set-NetAdapterAdvancedProperty'
- $updatePath= "C:\Optimize\Virtual-Desktop-Optimization-Tool-main\Win10_VirtualDesktop_Optimize.ps1"
+ $updatePath = "C:\Optimize\Virtual-Desktop-Optimization-Tool-main\Win10_VirtualDesktop_Optimize.ps1"
  ((Get-Content -path $updatePath -Raw) -replace 'Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB','#Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB') | Set-Content -Path $updatePath
  
  # Patch: overide the REG UNLOAD, needs GC before, otherwise will Access Deny unload(see readme.md)
- 
- [System.Collections.ArrayList]$file = Get-Content $updatePath
+  [System.Collections.ArrayList]$file = Get-Content $updatePath
  $insert = @()
  for ($i=0; $i -lt $file.count; $i++) {
    if ($file[$i] -like "*& REG UNLOAD HKLM\DEFAULT*") {
