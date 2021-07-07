9757bfcd18ab576f0488e30724285642517f0f97
rphoon <49342053+rphoon@users.noreply.github.com>
2021-07-08 01:08:26 +1000

    Update 1_Optimize_OS_for_WVD.ps1

--- 1_Optimize_OS_for_WVD.ps1
+++ 1_Optimize_OS_for_WVD.ps1
@@ -15,30 +15,30 @@
  Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-main
  
  # instrumentation
- $osOptURL = 'https://raw.githubusercontent.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/master/Win10_VirtualDesktop_Optimize.ps1'
- $osOptURLexe = 'optimize.ps1'
- Invoke-WebRequest -Uri $osOptURL -OutFile $osOptURLexe
+ #$osOptURL = 'https://raw.githubusercontent.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/master/Win10_VirtualDesktop_Optimize.ps1'
+ #$osOptURLexe = 'optimize.ps1'
+ #Invoke-WebRequest -Uri $osOptURL -OutFile $osOptURLexe
   
  # Patch: overide the Win10_VirtualDesktop_Optimize.ps1 - setting 'Set-NetAdapterAdvancedProperty'(see readme.md)
- Write-output 'Patch: Disabling Set-NetAdapterAdvancedProperty'
- $updatePath = "C:\Optimize\Virtual-Desktop-Optimization-Tool-main\Win10_VirtualDesktop_Optimize.ps1"
- ((Get-Content -path $updatePath -Raw) -replace 'Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB','#Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB') | Set-Content -Path $updatePath
+ #Write-output 'Patch: Disabling Set-NetAdapterAdvancedProperty'
+ #$updatePath = "C:\Optimize\Virtual-Desktop-Optimization-Tool-main\Win10_VirtualDesktop_Optimize.ps1"
+ #((Get-Content -path $updatePath -Raw) -replace 'Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB','#Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB') | Set-Content -Path $updatePath
  
  # Patch: overide the REG UNLOAD, needs GC before, otherwise will Access Deny unload(see readme.md)
-  [System.Collections.ArrayList]$file = Get-Content $updatePath
- $insert = @()
- for ($i=0; $i -lt $file.count; $i++) {
-   if ($file[$i] -like "*& REG UNLOAD HKLM\DEFAULT*") {
-     $insert += $i-1 
-   }
- }
+ # [System.Collections.ArrayList]$file = Get-Content $updatePath
+ #$insert = @()
+ #for ($i=0; $i -lt $file.count; $i++) {
+ #  if ($file[$i] -like "*& REG UNLOAD HKLM\DEFAULT*") {
+ #    $insert += $i-1 
+ #  }
+ #}
 
  #add gc and sleep
- $insert | ForEach-Object { $file.insert($_,"                 Write-Host 'Patch closing handles and runnng GC before reg unload' `n              `$newKey.Handle.close()` `n              [gc]::collect() `n                Start-Sleep -Seconds 15 ") }
- Set-Content $updatePath $file 
+ #$insert | ForEach-Object { $file.insert($_,"                 Write-Host 'Patch closing handles and runnng GC before reg unload' `n              `$newKey.Handle.close()` `n              [gc]::collect() `n                Start-Sleep -Seconds 15 ") }
+ #Set-Content $updatePath $file 
   
  # run script
  # .\optimize -WindowsVersion 2004 -Verbose
-  .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2009 -Verbose
-  Write-output 'AIB Customization: Finished OS Optimizations script'
+ .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2009 -Verbose
+ Write-output 'AIB Customization: Finished OS Optimizations script'
  
