b3b4576aa48ddf369017864412d1db9a949046b1
rphoon <49342053+rphoon@users.noreply.github.com>
2021-07-07 20:56:48 +1000

    Update 1_Optimize_OS_for_WVD.ps1

--- 1_Optimize_OS_for_WVD.ps1
+++ 1_Optimize_OS_for_WVD.ps1
@@ -1,5 +1,5 @@
  # OS Optimizations for WVD
- write-host 'AIB Customization: OS Optimizations for WVD'
+ Write-output 'AIB Customization: OS Optimizations for WVD'
  $appName = 'optimize'
  $drive = 'C:\'
  New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
@@ -9,10 +9,10 @@
  $osOptURLexe = 'Windows_10_VDI_Optimize-master.zip'
  $outputPath = $LocalPath + '\' + $osOptURLexe
  Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
- write-host 'AIB Customization: Starting OS Optimizations script'
+ Write-output 'AIB Customization: Starting OS Optimizations script'
  Expand-Archive -LiteralPath 'C:\\Optimize\\Windows_10_VDI_Optimize-master.zip' -DestinationPath $Localpath -Force -Verbose
  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
- Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-master
+ Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-main
  
  # instrumentation
  $osOptURL = 'https://raw.githubusercontent.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/master/Win10_VirtualDesktop_Optimize.ps1'
@@ -21,8 +21,8 @@
  
  
  # Patch: overide the Win10_VirtualDesktop_Optimize.ps1 - setting 'Set-NetAdapterAdvancedProperty'(see readme.md)
- Write-Host 'Patch: Disabling Set-NetAdapterAdvancedProperty'
- $updatePath= "C:\optimize\Virtual-Desktop-Optimization-Tool-master\Win10_VirtualDesktop_Optimize.ps1"
+ Write-output 'Patch: Disabling Set-NetAdapterAdvancedProperty'
+ $updatePath= "C:\Optimize\Virtual-Desktop-Optimization-Tool-main\Win10_VirtualDesktop_Optimize.ps1"
  ((Get-Content -path $updatePath -Raw) -replace 'Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB','#Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB') | Set-Content -Path $updatePath
  
  # Patch: overide the REG UNLOAD, needs GC before, otherwise will Access Deny unload(see readme.md)
@@ -39,10 +39,8 @@
  $insert | ForEach-Object { $file.insert($_,"                 Write-Host 'Patch closing handles and runnng GC before reg unload' `n              `$newKey.Handle.close()` `n              [gc]::collect() `n                Start-Sleep -Seconds 15 ") }
  Set-Content $updatePath $file 
   
-
- 
  # run script
  # .\optimize -WindowsVersion 2004 -Verbose
   .\Win10_VirtualDesktop_Optimize.ps1 -WindowsVersion 2009 -Verbose
-  write-host 'AIB Customization: Finished OS Optimizations script'
+  Write-output 'AIB Customization: Finished OS Optimizations script'
  
