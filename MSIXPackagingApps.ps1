7121d84fc0cde95bb8f28aff74afc864ccd9ebdd
rphoon <49342053+rphoon@users.noreply.github.com>
2025-01-21 12:24:40 +1100

    Update MSIXPackagingApps.ps1

--- MSIXPackagingApps.ps1
+++ MSIXPackagingApps.ps1
@@ -1,3 +1,10 @@
+# Apps for MSIX Packaging
+Write-Output 'AIB Customization: Install apps for MSIX Packaging'
+$appName = 'msixapps'
+$drive = 'C:\'
+New-Item -Path $drive -Name $appName -ItemType Directory -ErrorAction SilentlyContinue
+$LocalPath = $drive + '\' + $appName 
+Set-Location $LocalPath
 Write-Output '>>> Downloading MSIX Packaging Tool ...'
 Invoke-WebRequest -Uri "https://download.microsoft.com/download/e/2/e/e2e923b2-7a3a-4730-969d-ab37001fbb5e/MSIXPackagingtoolv1.2024.405.0.msixbundle" -OutFile "MSIXPackagingTool.msixbundle"
 Invoke-WebRequest -Uri "https://download.microsoft.com/download/e/2/e/e2e923b2-7a3a-4730-969d-ab37001fbb5e/MSIXPackagingtoolv1.2024.405.0.License.xml" -Outfile "License.xml"
