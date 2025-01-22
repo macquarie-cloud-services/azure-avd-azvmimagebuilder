7217781ca0cda1edc04485fe6043d37d55da20bc
rphoon <49342053+rphoon@users.noreply.github.com>
2025-01-22 11:04:10 +1100

    Update MSIXPackagingAppInstall.ps1

--- MSIXPackagingAppInstall.ps1
+++ MSIXPackagingAppInstall.ps1
@@ -7,11 +7,12 @@ $LocalPath = $drive + '\' + $appName
 Set-Location $LocalPath
 Write-Host 'AIB Customization: Downloading MSIX Packaging Tool ...'
 Invoke-WebRequest -Uri "https://download.microsoft.com/download/e/2/e/e2e923b2-7a3a-4730-969d-ab37001fbb5e/MSIXPackagingtoolv1.2024.405.0.msixbundle" -OutFile "MSIXPackagingTool.msixbundle"
-Invoke-WebRequest -Uri "https://download.microsoft.com/download/e/2/e/e2e923b2-7a3a-4730-969d-ab37001fbb5e/MSIXPackagingtoolv1.2024.405.0.License.xml" -Outfile "License.xml"
 Write-Host 'AIB Customization: Installing MSIX Packaging Tool ...'
-Start-Process powershell -ArgumentList "Add-AppxPackage -Path .\MSIXPackagingTool.msixbundle" -Verb RunAs -Wait
-#Write-Host '>>> Downloading MSIXHero ...'
-#Invoke-WebRequest -Uri "https://msixhero.net/msix-hero-3.1.0.0.msix" -Outfile "msix-hero.msix"
-#Write-Host '>>> Installing MSIXHero ...'
-#Start-Process powershell -ArgumentList "Add-AppxPackage -Path .\msix-hero.msix" -Verb RunAs -Wait
-Write-Host '>>> MSIX Packaging Apps installed ...'
+Add-AppxProvisionedPackage -Online -PackagePath 'MSIXPackagingTool.msixbundle' -SkipLicense
+Write-Host 'AIB Customization: Installing DotNET Desktop Runtime 8 (Prerequisite for MSIXHero) ...'
+winget install "Microsoft.DotNet.DesktopRuntime.8" --accept-source-agreements --accept-package-agreements --disable-interactivity
+Write-Host 'AIB Customization: Downloading MSIXHero ...'
+Invoke-WebRequest -Uri "https://msixhero.net/msix-hero-3.1.0.0.msix" -Outfile "msix-hero.msix"
+Write-Host 'AIB Customization: Installing MSIXHero ...'
+Add-AppxProvisionedPackage -Online -PackagePath 'msix-hero.msix' -SkipLicense
+Write-Host 'AIB Customization: MSIX Packaging Apps installed ...'
